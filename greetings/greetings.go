0b1f89431716a8cf855184eb2d1e05baae99e415
Gabriel Panassol <gpanassol@gmail.com>
2021-03-01 22:07:30 -0300

    Estudando tratamento de erro

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -1,7 +1,17 @@
 package greetings
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
-func Hello(name string) string {
-	return fmt.Sprintf("Hi, %v. Welcome!", name)
+func Hello(name string) (string, error) {
+
+	if name == "" {
+		return "", errors.New("empty name")
+	}
+
+	message := fmt.Sprintf("Hi, %v. Welcome!", name)
+
+	return message, nil
 }
